79ade1013e07aa381651a7b00f5f22d9495659bd
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-08-06 23:35:02 +0200

    Renamed template

--- paiji2_shoutbox/modular.py
+++ paiji2_shoutbox/modular.py
@@ -5,14 +5,14 @@ from modular_blocks import ModuleApp, TemplateTagBlock, modules
 from . import urls
 
 
-class BulletinBoardModule(ModuleApp):
+class ShoutboxModule(ModuleApp):
     app_name = 'bulletin_board'
     name = 'bulletin-board'
     urls = url(r'bulletin-board/', include(urls))
     templatetag_blocks = [
         TemplateTagBlock(
             name='bulletin-board',
-            library='bulletin',
+            library='shoutbox',
             tag='display_bulletin_board',
             cache_time=30 * 60,
             kwargs={
@@ -20,4 +20,6 @@ class BulletinBoardModule(ModuleApp):
             },
         ),
     ]
-modules.register(BulletinBoardModule)
+
+
+modules.register(ShoutboxModule)
