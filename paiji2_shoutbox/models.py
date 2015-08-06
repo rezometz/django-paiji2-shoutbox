41d545cb4fe16d1432834a6948e7b49b3d0a5c16
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-08-06 18:08:06 +0200

    Workaround for get_user_model with Django>=1.7

--- paiji2_shoutbox/models.py
+++ paiji2_shoutbox/models.py
@@ -1,12 +1,18 @@
 from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils.timezone import now
-from django.contrib.auth import get_user_model
+
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+except:
+    from django.contrib.auth.models import User
 
 
 class Note(models.Model):
     author = models.ForeignKey(
-        get_user_model(),
+        User,
         verbose_name=_('author'),
         related_name='notes',
     )
