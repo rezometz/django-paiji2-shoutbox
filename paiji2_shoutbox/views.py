9829e22faad570593892ba0a33000a9d3460a421
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-09-01 16:13:09 +0200

    uses SuccessUrlMixin

--- paiji2_shoutbox/views.py
+++ paiji2_shoutbox/views.py
@@ -1,12 +1,39 @@
 from django.views import generic
-# from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseNotFound
 from django.utils.translation import ugettext as _
-from django.conf import settings
-from django.contrib import messages
+# from django.conf import settings
+# from django.contrib import messages
+
+from paiji2_utils.views import SuccessUrlMixin
 
 from .models import Note
 from .forms import NoteForm
+
+
+class MySuccessUrl(SuccessUrlMixin):
+
+    default_url_name = 'message-list'
+
+    def get_forbidden_urls(self):
+        if hasattr(self, 'object'):
+            return (
+                reverse('message-edit', args=[self.object.pk]),
+                reverse('message-delete', args=[self.object.pk]),
+            )
+        else:
+            return None
+
+
+class OwnershipCheck(object):
+    def dispatch(self, request, *args, **kwargs):
+        """ Making sure that only authors can update notes """
+        obj = self.get_object()
+        if obj.author != self.request.user:
+            return HttpResponseNotFound(
+                _('Rezo is not hacked. You don\'t have the permission xD')
+            )
+        return super(OwnershipCheck, self).dispatch(request, *args, **kwargs)
 
 
 class NoteListView(generic.ListView):
@@ -28,34 +55,7 @@ class NoteListView(generic.ListView):
         )
 
 
-class MessageRedirect(object):
-    message_success = None
-
-    def get_success_url(self):
-        if self.message_success is not None:
-            messages.success(
-                self.request,
-                self.message_success,
-            )
-        success_url = self.request.POST.get('next', None)
-        if success_url is not None:
-            return success_url
-        else:
-            return settings.REDIRECT_URL
-
-
-class OwnershipCheck(object):
-    def dispatch(self, request, *args, **kwargs):
-        """ Making sure that only authors can update notes """
-        obj = self.get_object()
-        if obj.author != self.request.user:
-            return HttpResponseNotFound(
-                _('Rezo is not hacked. You don\'t have the permission xD')
-            )
-        return super(OwnershipCheck, self).dispatch(request, *args, **kwargs)
-
-
-class NoteCreateView(MessageRedirect, generic.CreateView):
+class NoteCreateView(MySuccessUrl, generic.CreateView):
     model = Note
     fields = ('message', )
     message_success = _(
@@ -63,13 +63,16 @@ class NoteCreateView(MessageRedirect, generic.CreateView):
         """it will be displayed in a moment"""
     )
     template_name = 'shoutbox/message/form.html'
+    forbidden_url_names = (
+        'message-add',
+    )
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(NoteCreateView, self).form_valid(form)
 
 
-class NoteEditView(MessageRedirect, OwnershipCheck, generic.UpdateView):
+class NoteEditView(OwnershipCheck, MySuccessUrl, generic.UpdateView):
     model = Note
     fields = ('message', )
     template_name = 'shoutbox/message/form.html'
@@ -78,7 +81,7 @@ class NoteEditView(MessageRedirect, OwnershipCheck, generic.UpdateView):
     )
 
 
-class NoteDeleteView(MessageRedirect, OwnershipCheck, generic.DeleteView):
+class NoteDeleteView(OwnershipCheck, MySuccessUrl, generic.DeleteView):
     model = Note
     template_name = 'shoutbox/note_confirm_delete.html'
     message_success = _(
