553f1f2b4643cf16d8aafc34d262d08c8e8c3b98
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2015-09-03 16:09:37 +0200

    adds an admin interface

--- paiji2_shoutbox/admin.py
+++ paiji2_shoutbox/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from paiji2_shoutbox.models import Note
+
+
+class ShoutboxAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'message',
+        'posted_at',
+    )
+
+
+admin.site.register(Note, ShoutboxAdmin)
